eeaf498ba747d371d37d958e8e32e4cdd3a4d053
Примаков Максим Антонович <spartmenik@gmail.com>
2025-04-09 23:40:05 +0300

    fixes

--- src/main/kotlin/ru/makcpp/randomblock/util/CamelToSnakeConverter.kt
+++
@@ -1,8 +0,0 @@
-package ru.makcpp.randomblock.util
-
-fun String.camelToSnakeCase(): String = buildString {
-    this@camelToSnakeCase.forEach { c ->
-        val lowerC = c.lowercase()
-        append(if (c.isUpperCase() && this.isNotEmpty()) "_$lowerC" else lowerC)
-    }
-}
--- src/main/kotlin/ru/makcpp/randomblock/util/Utils.kt
+++ src/main/kotlin/ru/makcpp/randomblock/util/Utils.kt
@@ -3,3 +3,10 @@ package ru.makcpp.randomblock.util
 import net.minecraft.world.World
 
 fun World.isServer(): Boolean = !isClient
+
+fun String.camelToSnakeCase(): String = buildString {
+    this@camelToSnakeCase.forEach { c ->
+        val lowerC = c.lowercase()
+        append(if (c.isUpperCase() && this.isNotEmpty()) "_$lowerC" else lowerC)
+    }
+}
